354e4296aaa79115de0fa4008d19cd9355bedf93
Wissemcreator <wissemnefzi5@gmail.com>
2025-07-10 21:14:08 +0200

    falco index

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
-// JS con tutti i paesi, G_Locale, fonti e link
+
+const globalData = {
+  gold: 24.5,
+  btc: 23.8,
+  countries: {
+    IT: {
+      name: "Italia",
+      local: 22.0,
+      comment: "Alta dipendenza da export agroalimentare",
+      source: "https://www.fao.org/italy"
+    },
+    DE: {
+      name: "Germania",
+      local: 23.2,
+      comment: "Industria forte, filiera interna stabile",
+      source: "https://www.worldbank.org/germany"
+    },
+    FR: {
+      name: "Francia",
+      local: 21.9,
+      comment: "Settore agricolo ampio ma meno efficiente",
+      source: "https://www.oecd.org/france"
+    }
+  }
+};
--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// Script dinamico calcolo Falco + interazioni
+
+function toggleInfo() {
+  document.getElementById("infoPanel").classList.toggle("hidden");
+}
+const map = L.map('map').setView([20, 0], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 5, minZoom: 2
+}).addTo(map);
+for (let code in globalData.countries) {
+  fetch(`https://nominatim.openstreetmap.org/search?country=${code}&format=json`)
+    .then(res => res.json()).then(data => {
+      if (data[0]) {
+        const lat = data[0].lat, lon = data[0].lon;
+        const val = globalData.countries[code].local;
+        const color = val > 23 ? "green" : val > 22 ? "orange" : "red";
+        const marker = L.circleMarker([lat, lon], {
+          radius: 8, fillColor: color, fillOpacity: 0.7, color: "#000", weight: 1
+        }).addTo(map);
+        marker.on('click', () => showCountry(code));
+      }
+    });
+}
+function showCountry(code) {
+  const c = globalData.countries[code];
+  const g = globalData;
+  const falco = ((g.gold + g.btc + c.local) / 3).toFixed(2);
+  document.getElementById("countryData").classList.remove("hidden");
+  document.getElementById("countryName").innerText = c.name;
+  document.getElementById("goldVal").innerText = g.gold;
+  document.getElementById("btcVal").innerText = g.btc;
+  document.getElementById("localVal").innerText = c.local;
+  document.getElementById("falcoVal").innerText = falco;
+  document.getElementById("comment").innerText = c.comment;
+  document.getElementById("sourceLink").href = c.source;
+}
+(function setGlobal() {
+  const g = globalData;
+  const avg = Object.values(g.countries).map(c => (g.gold + g.btc + c.local)/3)
+    .reduce((a,b)=>a+b,0) / Object.keys(g.countries).length;
+  document.getElementById("globalValue").innerText = avg.toFixed(2);
+})();
